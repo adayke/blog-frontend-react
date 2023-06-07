c13e572046a81ac6038c083d9500e2323b1f7c30
Aday <>
2023-06-07 19:04:34 +0600

    Added post/auth store and added new features

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -7,16 +8,21 @@ import CssBaseLine from "@mui/material/CssBaseline";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <CssBaseLine />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
